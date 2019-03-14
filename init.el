19f5998fdaa5fdbd6e4d22ce091105006b17a585
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-03-14 23:13:17 +0200

    add start configuration frame

---
+++ init.el
@@ -0,0 +1,43 @@
+;;; init.el -- Initialization file. -*- lexical-binding: t; -*-
+
+;; Set up package
+(require 'package)
+(setq package-archives
+      '(("org" . "http://orgmode.org/elpa/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("melpa-stable" . "http://stable.melpa.org/packages/")
+        ("gnu" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Bootstrap use-package
+;; Install use-package if it's not already installed.
+;; use-package is used to configure the rest of the packages.
+(unless (or (package-installed-p 'use-package)
+            (package-installed-p 'diminish))
+  (package-refresh-contents)
+  (package-install 'use-package)
+  (package-install 'diminish))
+
+;; From use-package README
+(eval-when-compile (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+;;; Code:
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(when window-system
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1))
+
+;;; Personal Layer:
+(org-babel-load-file
+ (expand-file-name "settings.org" user-emacs-directory))
+
+;;; init.el ends here
