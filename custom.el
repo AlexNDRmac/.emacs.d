5d70572fa9a36799eb0fea0777e85633e9d7fc92
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-03-20 08:55:24 +0200

    add Treemacs, cleanup settings

--- custom.el
+++ custom.el
@@ -5,3 +5,17 @@
 ;;; Code:
 
 ;;; Common
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (treemacs use-package magit editorconfig doom-themes dockerfile-mode docker-compose-mode diminish))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
