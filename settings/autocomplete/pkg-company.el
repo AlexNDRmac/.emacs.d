e09a5d40e9b5469fe14fd8e847a7c1e99fe6ca24
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-04-11 08:51:38 +0300

    modify Company pkg confoguration

--- settings/autocomplete/pkg-company.el
+++ settings/autocomplete/pkg-company.el
@@ -15,16 +15,43 @@
 
 ;; use Company
 (use-package company
-    :custom
-    (company-tooltip-limit 20)
-    (company-idle-delay .15)
-    (company-echo-delay 0)
+    :defer t
+    :init
+    (progn
+        (setq
+            company-tooltip-align-annotations t
+            company-tooltip-limit 20
+            company-echo-delay 0
+            company-idle-delay .15
+            company-show-numbers t
+            )))
 
-    :config
-    (setq company-begin-commands '(self-insert-command))
+;; Bind Keys for Company
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous)
+  (define-key company-active-map (kbd "SPC") #'company-abort))
 
-    (define-key company-active-map (kbd "C-n") #'company-select-next)
-    (define-key company-active-map (kbd "C-p") #'company-select-previous))
+;; Use Help
+(use-package company-quickhelp
+  :if (display-graphic-p)
+  :commands company-quickhelp-manual-begin
+  :init
+  (progn
+    (use-package pos-tip
+      :defer t
+      :pin melpa)
+
+    (with-eval-after-load 'company
+      (setq company-frontends
+            (delq 'company-echo-metadata-frontend company-frontends)
+            company-quickhelp-use-propertized-text t
+            company-quickhelp-max-lines 10
+            company-quickhelp-delay 0.3)
+      (company-quickhelp-mode)
+    )))
 
 (provide 'pkg-company)
 
