abe3e4d04265bcbbf9858e3296a3796c410ca8ef
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-06-02 11:57:21 +0300

    add Dired ablity to show directory first on Mac OS X

--- settings/appearance/conf-dired.el
+++ settings/appearance/conf-dired.el
@@ -14,11 +14,17 @@
 ;; Customization for Emacs Dired
 
 ;;; Code:
+
+;; Use Definitions
+(eval-when-compile
+  (require 'core-includes
+           (concat user-emacs-directory "settings/core/core-includes.el")))
+
 (defvar dired-mode-map)
+(defvar ls-lisp-use-insert-directory-program)
 
 ;; Look up file contents in other window while browsing directory in Dired.
 (use-package peep-dired
-  :ensure t
   :bind (:map dired-mode-map ("P" . peep-dired))
   :config
   (setq peep-dired-cleanup-on-disable t
@@ -28,13 +34,11 @@
 
 ;; Colorful file names in Dired buffers.
 (use-package diredful
-  :ensure t
   :config
   (diredful-mode 1))
 
 ;; Edit Filename At Point in an Emacs' dired buffer
 (use-package dired-efap
-  :ensure t
   :bind (:map dired-mode-map
               ([f2] . dired-efap)
               ([down-mouse-1] . dired-efap-click))
@@ -47,8 +51,23 @@
   (setq dired-listing-switches "-alh"  ; show human readable sizes in dired
         directory-free-space-args "-h" ; show human readable free space
         dired-recursive-deletes 'always
-        dired-recursive-copies 'always
-        ))
+        dired-recursive-copies 'always)
+
+  (when sys/macp
+    ;; Suppress warning: `ls does not support --dired'.
+    (setq dired-use-ls-dired nil)
+
+    (when (executable-find "gls")
+      ;; Use GNU `gls' from `coreutils' if available
+      (setq insert-directory-program "/usr/local/bin/gls")))
+
+  (when (or (and sys/macp (executable-find "gls"))
+            (and (not sys/macp) (executable-find "ls")))
+    ;; Using `insert-directory-program'
+    (setq ls-lisp-use-insert-directory-program t))
+
+  ;; Show directory first
+  (setq dired-listing-switches "-alh --group-directories-first"))
 
 (provide 'conf-dired)
 
