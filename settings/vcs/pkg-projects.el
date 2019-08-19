3d34ae575fd27e8591b64fb26f46e051dca9e8c0
AlexNDRmac <AlexNDR.mac@gmail.com>
2019-08-19 18:09:09 +0300

    minor improvement for projectile

--- settings/vcs/pkg-projects.el
+++ settings/vcs/pkg-projects.el
@@ -21,15 +21,15 @@
     projectile-completion-system 'ivy
     ;; Store bookmark file into .cache dir
     projectile-known-projects-file (concat user-cache-dir "projectile-bookmarks.eld")
+    projectile-cache-file (concat user-cache-dir "projectile.cache")
     ;; Enable Cache
     projectile-enable-caching t
-    projectile-cache-file (concat user-cache-dir "var/projectile.cache")
     ;; Ignores
     projectile-globally-ignored-files '(".DS_Store" "Icon" "TAGS")
     projectile-globally-ignored-file-suffixes
         '(".elc" ".pyc" ".o" ".lo" ".la" ".out" ".sock"))
   :config
-  (projectile-mode))
+  (projectile-mode +1))
 
 (provide 'pkg-projects)
 
